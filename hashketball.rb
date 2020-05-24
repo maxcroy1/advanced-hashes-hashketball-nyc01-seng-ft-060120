316e037e814d93e9b4d41afc73a9f908df47158b
Maxwell Croy <max.croy1@gmail.com>
2020-05-24 14:40:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,6 +187,7 @@ def player_stats(player)
   game_hash.each do |key, value|
     value[:players].each do |inner_key|
       if inner_key[:player_name] == player
+        stats = inner_key
       end
       binding.pry
     end
